1ae14e185a3691b94b96a1d7dc5fc5d157c116d8
Mohammed Alhassan <bigboss200535@gmail.com>
2024-05-08 17:13:20 +0000

    update

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -59,7 +59,7 @@
                                           <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
                                               <i class="bx bx-dots-vertical-rounded"></i>
                                           </button>
-                                              <div class="dropdown-menu">
+                                            <div class="dropdown-menu">
                                                   <a class="dropdown-item" href="">
                                                     <i class="bx bx-edit-alt me-1"></i> More
                                                   </a>
@@ -67,8 +67,7 @@
                                                       <i class="bx bx-trash me-1"></i> Delete
                                                   </a>
                                             </div>
-                                  </div>  
-
+                                </div>  
                               </td>
                           </tr>
                             @endforeach
